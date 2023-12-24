6c59dd9a103f1dbe3079ffde7ace17703548e360
KimA-Mik <ananim38@gmail.com>
2023-12-25 01:18:08 +0300

    Had fun with DialogPreview

--- app/src/main/java/ru/kima/intelligentchat/presentation/common/dialogs/SimpleAlertDialog.kt
+++ app/src/main/java/ru/kima/intelligentchat/presentation/common/dialogs/SimpleAlertDialog.kt
@@ -1,11 +1,19 @@
 package ru.kima.intelligentchat.presentation.common.dialogs
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Dialpad
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import ru.kima.intelligentchat.presentation.ui.theme.IntelligentChatTheme
 
 @Composable
 fun SimpleAlertDialog(
@@ -32,4 +40,25 @@ fun SimpleAlertDialog(
                 Text(text = dismissText)
             }
         })
+}
+
+
+@Preview(name = "Normal theme")
+@Preview(
+    name = "Night theme",
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun DialogPreview() {
+    IntelligentChatTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            SimpleAlertDialog(
+                onConfirm = {  },
+                onDismiss = {  },
+                title = "Dialog",
+                text = "Text",
+                icon = Icons.Outlined.Dialpad
+            )
+        }
+    }
 }
