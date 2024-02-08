c9de84a5fa03f7d6053aec0666b3e4a6a57c1e9f
KimA-Mik <ananim38@gmail.com>
2024-02-08 22:45:01 +0300

    Bump dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,13 +65,13 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended-android:1.6.0")
+    implementation("androidx.compose.material:material-icons-extended-android:1.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation(project(mapOf("path" to ":domain")))
     implementation(project(mapOf("path" to ":data")))
     implementation(project(mapOf("path" to ":core")))
 
-    val navVersion = "2.7.6"
+    val navVersion = "2.7.7"
     implementation("androidx.navigation:navigation-compose:$navVersion")
 
     val koinVersion = "3.5.3"
