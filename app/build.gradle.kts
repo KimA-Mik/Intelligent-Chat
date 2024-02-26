caa2f98decd1b0d70d3b3b3c19117360de5eb6e8
KimA-Mik <ananim38@gmail.com>
2024-02-26 18:40:34 +0300

    Version 0.72

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 7
-        versionName = "0.7"
+        versionName = "0.72"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
