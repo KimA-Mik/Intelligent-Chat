8a05bb86270ac8443f73c27dc7dcd99962a5848f
KimA-Mik <ananim38@gmail.com>
2024-04-10 17:39:27 +0300

    Add generic SimpleDropdownMenu

---
+++ app/src/main/java/ru/kima/intelligentchat/presentation/connection/presets/horde/edit/DropdownMenuItems.kt
@@ -0,0 +1,11 @@
+package ru.kima.intelligentchat.presentation.connection.presets.horde.edit
+
+import ru.kima.intelligentchat.R
+import ru.kima.intelligentchat.presentation.ui.components.SimpleDropDownMenuItem
+
+val dropDownMenuItems = listOf(
+    SimpleDropDownMenuItem(
+        R.string.save_preset,
+        onClick = {}
+    )
+)
