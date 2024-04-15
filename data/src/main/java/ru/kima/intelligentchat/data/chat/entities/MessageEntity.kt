a83771cb718bf95c86c77c6a4a891bc2ce6b3e96
KimA-Mik <ananim38@gmail.com>
2024-04-15 16:46:28 +0300

    Add SenderType to MessageEntity

--- data/src/main/java/ru/kima/intelligentchat/data/chat/entities/MessageEntity.kt
+++ data/src/main/java/ru/kima/intelligentchat/data/chat/entities/MessageEntity.kt
@@ -2,13 +2,17 @@ package ru.kima.intelligentchat.data.chat.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
 import ru.kima.intelligentchat.data.MESSAGES_TABLE_NAME
+import ru.kima.intelligentchat.data.chat.types.sender.SenderType
+import ru.kima.intelligentchat.data.chat.types.sender.SenderTypeConverter
 
 @Entity(tableName = MESSAGES_TABLE_NAME)
 data class MessageEntity(
     @PrimaryKey(autoGenerate = true)
     val messageId: Long,
-    val sender: Int,
+    @TypeConverters(SenderTypeConverter::class)
+    val sender: SenderType,
     val senderId: Long,
     val index: Int,
     val selectedSwipeIndex: Int
