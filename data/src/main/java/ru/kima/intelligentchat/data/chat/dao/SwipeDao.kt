d12c48b3fea9af970059e48035b5ef4393bed45d
KimA-Mik <ananim38@gmail.com>
2024-04-19 23:15:54 +0300

    Prepare to migrate all the logic to domain layer

--- data/src/main/java/ru/kima/intelligentchat/data/chat/dao/SwipeDao.kt
+++ data/src/main/java/ru/kima/intelligentchat/data/chat/dao/SwipeDao.kt
@@ -2,7 +2,10 @@ package ru.kima.intelligentchat.data.chat.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+import ru.kima.intelligentchat.data.SWIPE_TABLE_NAME
 import ru.kima.intelligentchat.data.chat.entities.SwipeEntity
 
 @Dao
@@ -12,4 +15,7 @@ interface SwipeDao {
 
     @Update
     suspend fun updateSwipe(swipeEntity: SwipeEntity)
+
+    @Query("SELECT * FROM $SWIPE_TABLE_NAME WHERE message_id in (:messagesIds)")
+    fun selectSwipesForMessages(messagesIds: List<Long>): Flow<List<SwipeEntity>>
 }
