4924689d3a97b450a30759e2980278c8d143a974
KimA-Mik <ananim38@gmail.com>
2024-04-22 20:48:16 +0300

    implement swipesForChat to SwipeDao, for previous commit

--- data/src/main/java/ru/kima/intelligentchat/data/chat/dao/SwipeDao.kt
+++ data/src/main/java/ru/kima/intelligentchat/data/chat/dao/SwipeDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
+import ru.kima.intelligentchat.data.MESSAGES_TABLE_NAME
 import ru.kima.intelligentchat.data.SWIPE_TABLE_NAME
 import ru.kima.intelligentchat.data.chat.entities.SwipeEntity
 
@@ -17,5 +18,11 @@ interface SwipeDao {
     suspend fun updateSwipe(swipeEntity: SwipeEntity)
 
     @Query("SELECT * FROM $SWIPE_TABLE_NAME WHERE message_id in (:messagesIds)")
-    fun selectSwipesForMessages(messagesIds: List<Long>): Flow<List<SwipeEntity>>
+    fun swipesForMessages(messagesIds: List<Long>): Flow<List<SwipeEntity>>
+
+    @Query(
+        "SELECT * FROM $SWIPE_TABLE_NAME WHERE message_id in " +
+                "(SELECT message_id FROM $MESSAGES_TABLE_NAME WHERE chat_id = :chatId)"
+    )
+    fun swipesForChat(chatId: Long): Flow<List<SwipeEntity>>
 }
