75a7f5704c67cab287f84e38ae4410de13217734
KimA-Mik <ananim38@gmail.com>
2024-05-03 21:35:47 +0300

    Update CardImage

--- app/src/main/java/ru/kima/intelligentchat/presentation/characterCard/cardDetails/components/CardImage.kt
+++ app/src/main/java/ru/kima/intelligentchat/presentation/characterCard/cardDetails/components/CardImage.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person4
@@ -11,82 +12,83 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.kima.intelligentchat.presentation.common.image.ImmutableBitmap
+import ru.kima.intelligentchat.presentation.common.image.ImmutableImageBitmap
 import ru.kima.intelligentchat.presentation.ui.theme.IntelligentChatTheme
 
 @Composable
 fun CardImage(
     image: ImmutableBitmap,
     modifier: Modifier = Modifier,
+    emptyImageVector: ImageVector = Icons.Default.Person4,
+    contentDescription: String? = null,
     onClick: () -> Unit
 ) {
-//    val bitmap = remember(photoBytes) {
-//        if (photoBytes != null)
-//            BitmapFactory.decodeByteArray(photoBytes, 0, photoBytes.size).asImageBitmap()
-//        else
-//            null
-//    }
-
-    var bitmap by remember {
-        mutableStateOf<ImageBitmap?>(null)
+    val bitmap by remember(image) {
+        val imageBitmap = image.bitmap?.asImageBitmap()
+        imageBitmap?.prepareToDraw()
+        mutableStateOf(ImmutableImageBitmap(imageBitmap))
     }
 
-    LaunchedEffect(image) {
-        bitmap = image.bitmap?.asImageBitmap()
-    }
+    CardImage(
+        bitmap = bitmap,
+        modifier = modifier,
+        emptyImageVector = emptyImageVector,
+        contentDescription = contentDescription,
+        onClick = onClick
+    )
+}
 
+@Composable
+fun CardImage(
+    bitmap: ImmutableImageBitmap,
+    modifier: Modifier = Modifier,
+    emptyImageVector: ImageVector = Icons.Default.Person4,
+    contentDescription: String? = null,
+    onClick: () -> Unit
+) {
+    val imageModifier = modifier
+        .clip(CircleShape)
+        .border(
+            1.5.dp,
+            MaterialTheme.colorScheme.onSecondaryContainer,
+            CircleShape
+        )
+        .clickable {
+            onClick()
+        }
 
-    if (bitmap == null) {
+    if (bitmap.imageBitmap == null) {
         val iconBgColor = MaterialTheme.colorScheme.secondaryContainer
         Icon(
-            Icons.Filled.Person4,
-            contentDescription = "",
+            imageVector = emptyImageVector,
+            contentDescription = contentDescription,
             tint = MaterialTheme.colorScheme.onSecondaryContainer,
-            modifier = modifier
-                .clip(CircleShape)
-                .border(
-                    1.5.dp,
-                    MaterialTheme.colorScheme.onSecondaryContainer,
-                    CircleShape
-                )
+            modifier = imageModifier
                 .drawBehind {
                     drawCircle(color = iconBgColor)
-                }
-                .clickable {
-                    onClick()
                 }
         )
     } else {
         Image(
-            painter = BitmapPainter(bitmap!!), contentDescription = "",
-            modifier = modifier
-                .clip(CircleShape)
-                .border(
-                    1.5.dp,
-                    MaterialTheme.colorScheme.onSecondaryContainer,
-                    CircleShape
-                )
-                .clickable {
-                    onClick()
-                },
+            painter = BitmapPainter(bitmap.imageBitmap),
+            contentDescription = contentDescription,
+            modifier = imageModifier,
             contentScale = ContentScale.Crop
         )
     }
-
 }
 
 @Preview(name = "CardImage preview light theme")
@@ -98,9 +100,11 @@ fun CardImage(
 fun CardImagePreview() {
     IntelligentChatTheme {
         Surface(color = MaterialTheme.colorScheme.background) {
-            CardImage(image = ImmutableBitmap()) {
-
-            }
+            CardImage(
+                image = ImmutableBitmap(),
+                modifier = Modifier.padding(8.dp),
+                onClick = {}
+            )
         }
     }
 }
