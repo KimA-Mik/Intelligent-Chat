d45b0c0de496769e34617e990731ca58fa74c62c
KimA-Mik <ananim38@gmail.com>
2024-10-22 16:38:23 +0300

    Now SwipeFirstMessageUseCase changes selectedGreeting for chat

--- domain/src/main/java/ru/kima/intelligentchat/domain/chat/useCase/inChat/SwipeFirstMessageUseCase.kt
+++ domain/src/main/java/ru/kima/intelligentchat/domain/chat/useCase/inChat/SwipeFirstMessageUseCase.kt
@@ -2,19 +2,21 @@ package ru.kima.intelligentchat.domain.chat.useCase.inChat
 
 import kotlinx.coroutines.flow.first
 import ru.kima.intelligentchat.domain.card.useCase.GetCardUseCase
-import ru.kima.intelligentchat.domain.card.useCase.UpdateCardUseCase
 import ru.kima.intelligentchat.domain.chat.model.SwipeDirection
+import ru.kima.intelligentchat.domain.chat.repository.ChatRepository
 
 class SwipeFirstMessageUseCase(
     private val getCard: GetCardUseCase,
-    private val updateCard: UpdateCardUseCase
+    private val chatRepository: ChatRepository
 ) {
-    suspend operator fun invoke(cardId: Long, direction: SwipeDirection) {
+    suspend operator fun invoke(cardId: Long, chatId: Long, direction: SwipeDirection) {
         val card = try {
             getCard(cardId).first()
         } catch (e: NullPointerException) {
             return
         }
+
+        val chat = chatRepository.subscribeToChat(chatId).first()
 
         val totalGreetings = card.alternateGreetings.size + 1
         if (totalGreetings == 1) {
@@ -26,10 +28,10 @@ class SwipeFirstMessageUseCase(
             SwipeDirection.Right -> 1
         }
 
-        var newSwipe = card.selectedGreeting + add
+        var newSwipe = chat.selectedGreeting + add
         if (newSwipe < 0) newSwipe = totalGreetings - 1
         if (newSwipe >= totalGreetings) newSwipe = 0
 
-        updateCard(card.copy(selectedGreeting = newSwipe))
+        chatRepository.updateChat(chat.copy(selectedGreeting = newSwipe))
     }
 }
