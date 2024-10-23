a1e10535306e83354b003c2a9521e399ecba015a
KimA-Mik <ananim38@gmail.com>
2024-10-24 01:27:11 +0300

    Introduce GenerationRequest and constructPrompt function

---
+++ domain/src/main/java/ru/kima/intelligentchat/domain/messaging/generation/model/GenerationRequest.kt
@@ -0,0 +1,18 @@
+package ru.kima.intelligentchat.domain.messaging.generation.model
+
+import ru.kima.intelligentchat.domain.card.model.CharacterCard
+import ru.kima.intelligentchat.domain.chat.model.FullChat
+import ru.kima.intelligentchat.domain.chat.model.SenderType
+import ru.kima.intelligentchat.domain.persona.model.Persona
+import ru.kima.intelligentchat.domain.tokenizer.Tokenizer
+
+data class GenerationRequest(
+    val maxResponseLength: Int,
+    val maxContextLength: Int,
+    val persona: Persona,
+    val card: CharacterCard,
+    val chat: FullChat,
+    val stopSequence: List<String>,
+    val generateFor: SenderType,
+    val tokenizer: Tokenizer
+)
