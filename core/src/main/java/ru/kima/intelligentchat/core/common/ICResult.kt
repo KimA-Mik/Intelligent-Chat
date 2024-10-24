faf64d5fbafec49dbfa3fef2d0553fdfecf1cb15
KimA-Mik <ananim38@gmail.com>
2024-10-24 20:49:40 +0300

    Improve ICResult

--- core/src/main/java/ru/kima/intelligentchat/core/common/ICResult.kt
+++ core/src/main/java/ru/kima/intelligentchat/core/common/ICResult.kt
@@ -1,6 +1,14 @@
 package ru.kima.intelligentchat.core.common
 
-sealed interface ICResult<S, E> {
-    data class Success<S, E>(val data: S) : ICResult<S, E>
-    data class Error<S, E>(val data: E) : ICResult<S, E>
+
+sealed interface ICResult<out S, out E> {
+    data class Success<out S, E>(val data: S) : ICResult<S, E>
+    data class Error<S, out E>(val data: E) : ICResult<S, E>
+}
+
+inline fun <S, E> ICResult<S, E>.valueOr(alternative: (E) -> S): S {
+    return when (this) {
+        is ICResult.Error -> alternative(data)
+        is ICResult.Success -> data
+    }
 }
