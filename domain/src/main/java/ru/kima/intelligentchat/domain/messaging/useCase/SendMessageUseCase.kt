b8edc7d084fafea055df0bc213b22a4be1a74609
KimA-Mik <ananim38@gmail.com>
2024-11-08 20:37:36 +0300

    Add handling of regenerating message

--- domain/src/main/java/ru/kima/intelligentchat/domain/messaging/useCase/SendMessageUseCase.kt
+++ domain/src/main/java/ru/kima/intelligentchat/domain/messaging/useCase/SendMessageUseCase.kt
@@ -2,6 +2,7 @@ package ru.kima.intelligentchat.domain.messaging.useCase
 
 import kotlinx.coroutines.flow.first
 import ru.kima.intelligentchat.domain.chat.model.SenderType
+import ru.kima.intelligentchat.domain.chat.repository.MessageRepository
 import ru.kima.intelligentchat.domain.chat.useCase.InitializeChatUseCase
 import ru.kima.intelligentchat.domain.chat.useCase.inChat.CreateMessageUseCase
 import ru.kima.intelligentchat.domain.messaging.repositoty.MessagingRepository
@@ -12,17 +13,25 @@ class SendMessageUseCase(
     private val initializeChat: InitializeChatUseCase,
     private val messagingRepository: MessagingRepository,
     private val getSelectedPersona: SelectedPersonaUseCase,
+    private val messageRepository: MessageRepository,
 ) {
     suspend operator fun invoke(
         chatId: Long,
         personaId: Long,
         text: String
     ) {
-        if (text.isBlank()) return
         val selectedPersona = getSelectedPersona().first()
         initializeChat(chatId, selectedPersona)
 
-        createMessage(chatId, SenderType.Persona, personaId, text)
+        if (text.isNotBlank()) {
+            createMessage(chatId, SenderType.Persona, personaId, text.trim())
+        } else {
+            val messages = messageRepository.subscribeToChatMessages(chatId).first()
+            if (messages.isEmpty()) return
+            //That's job for impersonation
+            if (messages.last().sender == SenderType.Character) return
+        }
+
         messagingRepository.initiateGeneration(
             chatId = chatId,
             personaId = selectedPersona.id,
