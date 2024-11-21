860e4bf76a8dbc2afb9fe237f46311149ba36a26
KimA-Mik <ananim38@gmail.com>
2024-11-22 01:13:33 +0300

    Bump versionName to "0.85.2"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 12
-        versionName = "0.85.1"
+        versionName = "0.85.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
