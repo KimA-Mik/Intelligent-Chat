1c4c79e7a3b554aba13f052087c51a1363d921b2
KimA-Mik <ananim38@gmail.com>
2024-12-10 17:26:24 +0300

    Factor out notifications to NotificationHandler

--- app/src/main/java/ru/kima/intelligentchat/presentation/android/notifications/NotificationHandler.kt
+++ app/src/main/java/ru/kima/intelligentchat/presentation/android/notifications/NotificationHandler.kt
@@ -8,50 +8,62 @@ import android.graphics.Bitmap
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
-import androidx.core.graphics.drawable.IconCompat
 import ru.kima.intelligentchat.R
 
-class NotificationHandler(context: Context) {
+class NotificationHandler(
+    private val context: Context
+) {
     private val notificationManager =
         context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
     init {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            initializeNotificationChannels(context)
+            notificationManager.createNotificationChannel(awaitingForMessageNotificationChannel())
+            notificationManager.createNotificationChannel(generationErrorNotificationChannel())
         }
     }
 
     fun getCharacterTypingNotification(
-        context: Context,
         characterName: String,
         characterImage: Bitmap? = null
     ): Notification {
         val title = context.getString(R.string.awaiting_for_message_notification, characterName)
-        val icon = characterImage?.let {
-            IconCompat.createWithBitmap(it)
-        }
-
         val builder = NotificationCompat
-            .Builder(context, AWAITING_FOR_MESSAGE_CHANNEL_ID)
+            .Builder(context, AWAITING_FOR_MESSAGE_NOTIFICATION_CHANNEL_ID)
             .setContentTitle(title)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
 
-        icon?.let { builder.setSmallIcon(icon) }
-
+        characterImage?.let { builder.setLargeIcon(it) }
         return builder.build()
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun initializeNotificationChannels(context: Context) {
+    private fun awaitingForMessageNotificationChannel(): NotificationChannel {
         val name = context.getString(R.string.awaiting_for_message_notification_channel_name)
-        val description =
+        val importance = NotificationManager.IMPORTANCE_LOW
+        val channel =
+            NotificationChannel(AWAITING_FOR_MESSAGE_NOTIFICATION_CHANNEL_ID, name, importance)
+        channel.description =
             context.getString(R.string.awaiting_for_message_notification_channel_description)
+        return channel
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun generationErrorNotificationChannel(): NotificationChannel {
+        val name = context.getString(R.string.generation_error_notification_channel_name)
         val importance = NotificationManager.IMPORTANCE_DEFAULT
-        val channel = NotificationChannel(AWAITING_FOR_MESSAGE_CHANNEL_ID, name, importance)
-        channel.description = description
-        notificationManager.createNotificationChannel(channel)
+        val channel =
+            NotificationChannel(GENERATION_ERROR_NOTIFICATIONS_CHANNEL_ID, name, importance)
+        channel.description =
+            context.getString(R.string.generation_error_notification_channel_description)
+        return channel
     }
 
     companion object {
-        private const val AWAITING_FOR_MESSAGE_CHANNEL_ID = "awaiting_for_message_channel_id"
+        private const val AWAITING_FOR_MESSAGE_NOTIFICATION_CHANNEL_ID =
+            "awaiting_for_message_notification_channel"
+        private const val CHAT_MESSAGE_NOTIFICATIONS_CHANNEL_ID = "chat_message_notifications"
+        private const val GENERATION_ERROR_NOTIFICATIONS_CHANNEL_ID =
+            "generation_error_notifications"
     }
 }
