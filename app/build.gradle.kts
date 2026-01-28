38a0f128003f86259c9ac9746347fe238c7e026e
KimA-Mik <ananim38@gmail.com>
2026-01-28 19:14:44 +0300

    Bump versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 13
-        versionName = "0.88.2"
+        versionName = "0.88.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
