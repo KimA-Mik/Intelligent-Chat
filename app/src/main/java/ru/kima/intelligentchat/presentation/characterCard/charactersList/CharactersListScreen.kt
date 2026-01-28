be6cc2aaa3b869f974c72e7279b6938280f5f3de
KimA-Mik <ananim38@gmail.com>
2026-01-28 20:02:14 +0300

    Replace self made fab menu with m3 expressive version

--- app/src/main/java/ru/kima/intelligentchat/presentation/characterCard/charactersList/CharactersListScreen.kt
+++ app/src/main/java/ru/kima/intelligentchat/presentation/characterCard/charactersList/CharactersListScreen.kt
@@ -1,6 +1,7 @@
 package ru.kima.intelligentchat.presentation.characterCard.charactersList
 
 import android.content.res.Configuration
+import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
@@ -25,29 +26,45 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.FloatingActionButtonMenu
+import androidx.compose.material3.FloatingActionButtonMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.ToggleFloatingActionButton
+import androidx.compose.material3.ToggleFloatingActionButtonDefaults.animateIcon
+import androidx.compose.material3.TooltipAnchorPosition
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.animateFloatingActionButton
 import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.retain.retain
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -103,7 +120,7 @@ fun CharactersListScreenRoot(
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 private fun CharactersListScreen(
     state: CharactersListState,
@@ -177,7 +194,9 @@ private fun CharactersListScreen(
         )
     }
 
-    var isExpanded by remember { mutableStateOf(false) }
+    var isExpanded by retain { mutableStateOf(false) }
+    BackHandler(isExpanded) { isExpanded = false }
+
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
     Scaffold(
         topBar = {
@@ -190,8 +209,9 @@ private fun CharactersListScreen(
             )
         },
         floatingActionButton = {
-            ActionButtons(
+            FabMenu(
                 isExpanded,
+                fabVisible = true,
                 onImageButtonClick = {
                     onEvent(CharactersListUserEvent.AddCardFromImageClicked)
                 },
@@ -200,7 +220,20 @@ private fun CharactersListScreen(
                 },
                 onControlButtonClick = {
                     isExpanded = !isExpanded
-                })
+                }
+            )
+
+//            ActionButtons(
+//                isExpanded,
+//                onImageButtonClick = {
+//                    onEvent(CharactersListUserEvent.AddCardFromImageClicked)
+//                },
+//                onCreateButtonClick = {
+//                    onEvent(CharactersListUserEvent.CreateCardClicked)
+//                },
+//                onControlButtonClick = {
+//                    isExpanded = !isExpanded
+//                })
         },
         snackbarHost = { SnackbarHost(snackbarHostState) }) { paddingValues ->
         CharactersList(
@@ -343,6 +376,72 @@ fun InitPersonaDialog(
                 }
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
+@Composable
+private fun FabMenu(
+    isExpanded: Boolean,
+    fabVisible: Boolean,
+    modifier: Modifier = Modifier,
+    onImageButtonClick: () -> Unit,
+    onCreateButtonClick: () -> Unit,
+    onControlButtonClick: () -> Unit
+) {
+    val focusRequester = remember { FocusRequester() }
+    FloatingActionButtonMenu(
+        expanded = isExpanded,
+        button = {
+            TooltipBox(
+                positionProvider =
+                    TooltipDefaults.rememberTooltipPositionProvider(
+                        if (isExpanded) {
+                            TooltipAnchorPosition.Start
+                        } else {
+                            TooltipAnchorPosition.Above
+                        }
+                    ),
+                tooltip = { PlainTooltip { Text("Toggle menu") } },
+                state = rememberTooltipState(),
+            ) {
+                ToggleFloatingActionButton(
+                    modifier =
+                        Modifier
+                            .animateFloatingActionButton(
+                                visible = fabVisible || isExpanded,
+                                alignment = Alignment.BottomEnd,
+                            )
+                            .focusRequester(focusRequester),
+                    checked = isExpanded,
+                    onCheckedChange = { onControlButtonClick() },
+                ) {
+                    val imageVector by remember {
+                        derivedStateOf {
+                            if (checkedProgress > 0.5f) Icons.Filled.Close else Icons.Filled.Add
+                        }
+                    }
+                    Icon(
+                        painter = rememberVectorPainter(imageVector),
+                        contentDescription = null,
+                        modifier = Modifier.animateIcon({ checkedProgress }),
+                    )
+                }
+            }
+        },
+        modifier = modifier
+    ) {
+        FloatingActionButtonMenuItem(
+            onClick = onImageButtonClick,
+            text = { Text("Import from png") },
+            icon = { Icon(Icons.Filled.Image, contentDescription = "From image") }
+        )
+
+        FloatingActionButtonMenuItem(
+            onClick = onCreateButtonClick,
+            text = { Text("Create") },
+            icon = { Icon(Icons.Filled.Create, contentDescription = "Create Card") }
+        )
     }
 }
 
